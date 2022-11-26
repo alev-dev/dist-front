3526b5ae14c992c868805f1f098390a734015c5b
alejandro.alfonso1 <alejandro.alfonso@onlive.site>
2022-11-26 15:48:25 -0300

    a

--- src/components/CustomCard.jsx
+++ src/components/CustomCard.jsx
@@ -3,7 +3,7 @@ import Card from 'react-bootstrap/Card';
 import { useAppContext } from '../context/AppContext';
 
 function CustomCard({ product, buttonText }) {
-    const { image, name, description, price } = product;
+    const { image, name, description, price, stock } = product;
     const { addToCart } = useAppContext();
     return (
         <Card style={{ width: '18rem', margin: '10px 20px' }}>
@@ -14,7 +14,11 @@ function CustomCard({ product, buttonText }) {
                 <div className="dist-card-footer">
                     <span className="price">{`${price} BRL`}</span>
 
-                    <Button variant="primary" onClick={() => addToCart(product)}>
+                    <Button
+                        variant={stock > 0 ? 'primary' : 'danger'}
+                        disabled={stock < 1}
+                        onClick={() => addToCart(product)}
+                    >
                         {buttonText}
                     </Button>
                 </div>
